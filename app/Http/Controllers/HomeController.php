571a74570e6d059fca6dcf8312481ec2eed89d39
twill <twill1@gmx.com>
2020-07-04 11:50:30 -0500

    Home page additions

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Order;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class HomeController extends Controller
 {
@@ -23,6 +25,15 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $revenue = DB::select("SELECT SUM(order_subtotal) AS totalsum FROM orders");
+        $revenue = number_format($revenue[0]->totalsum, 2);
+        $totalOrders = count(Order::get());
+        $orders = Order::take(5)->orderBy('created_at', 'desc')->get();
+
+        return view('home', [
+            'orders' => $orders,
+            'totalOrders' => $totalOrders,
+            'revenue' => $revenue,
+        ]);
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,14 +2,67 @@
 
 @section('content')
 <div class="container">
+
+    <div class="row mb-4 justify-content-center">
+        <div class="col-5 col-md-4">
+            <div class="card shadow-sm">
+                <div class="card-body text-center">
+                    <h1 id="product-numbers-h1" class="font-weight-bold text-info">{{$totalOrders}}</h1>
+                    Total Orders
+                </div>
+            </div>
+        </div>
+        <div class="col-5 col-md-4">
+            <div class="card shadow-sm">
+                <div class="card-body text-center">
+                    <h1 id="product-worth-h1" class="font-weight-bold text-info">${{$revenue}}</h1>
+                    Revenue
+                </div>
+            </div>
+        </div>
+    </div>
+
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
+            <div class="card shadow-sm">
+
                 <div class="card-body">
-                    <a href="/products">Products</a><br>
-                    <a href="/orders">Orders</a><br>
-                    <a href="/customers">Customers</a>
+                    <div class="order-recent">
+                        <h3 class="text-muted">Recent Orders</h3>
+                        <div class="table-responsive-md">
+                            <table class="table table-hover">
+                                <thead>
+                                  <tr>
+                                    <th scope="col">ID</th>
+                                    <th scope="col">Ship To</th>
+                                    <th scope="col">Status</th>
+                                    <th scope="col">Date</th>
+                                    <th scope="col">Subtotal</th>
+                                    <th scope="col"></th>
+                                  </tr>
+                                </thead>
+                                <tbody>
+                                    
+                                    @foreach ($orders as $order)
+                                    <tr>
+                                        <th scope="row">{{$order->id}}</th>
+                                        <td>{{$order->ship_to}}</td>
+                                        <td>{{$order->status}}</td>
+                                        <td>{{date("F jS, Y", strtotime($order->created_at))}}</td>
+                                        <td>${{$order->order_subtotal}}</td>
+                                        <td class="text-center">
+                                            <a href="/orders/{{$order->id}}" class="btn-sm btn-primary remove-link-styling">View</a>
+                                        </td>
+                                    </tr>    
+                                    @endforeach
+    
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+
                 </div>
+
             </div>
         </div>
     </div>
