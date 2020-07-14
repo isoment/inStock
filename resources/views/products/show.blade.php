97bba3dcf95d80aab9e204768d2b1386430bb0a8
twill <twill1@gmx.com>
2020-07-14 17:17:46 -0500

    Fixed display error on product details

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -21,42 +21,6 @@
                                 <div class="mt-2">{{$product->description}}</div>
                             </h5>
                         </div>
-                    <h3 class="mt-5 text-muted font-weight-bold">Recent orders of {{$product->name}}</h3>
-                        <div class="table-responsive-md">
-                            <table class="table table-hover">
-                                <thead>
-                                <tr>
-                                    <th scope="col">ID</th>
-                                    <th scope="col">Status</th>
-                                    <th scope="col">Date</th>
-                                    <th scope="col">Product</th>
-                                    <th scope="col">Customer</th>
-                                    <th scope="col">Email</th>
-                                    <th scope="col"></th>
-                                </tr>
-                                </thead>
-                                <tbody>
-                                    
-                                    {{-- @foreach ($ordersWithProduct as $order)
-                                    <tr>
-                                        <th scope="row">{{$order->id}}</th>
-                                        <td>{{$order->status}}</td>
-                                        <td>{{date("F jS, Y", strtotime($order->created_at))}}</td>
-                                        <td>{{$order->product_name}}</td>
-                                        <td>{{$order->customer_name}}</td>
-                                        <td>{{$order->email}}</td>
-                                        <td class="text-center">
-                                            <a href="/orders/{{$order->id}}" class="btn-sm btn-primary remove-link-styling">View</a>
-                                        </td>
-                                    </tr>    
-                                    @endforeach --}}
-
-                                </tbody>
-                            </table>
-                            {{-- <div>
-                                {{ $ordersWithProduct->links() }}
-                            </div> --}}
-                        </div>
                     </div>
                 </div>
             </div>
